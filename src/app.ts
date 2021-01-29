9405e4992b09b25a9786272b5c73dba8a9f2a6b0
cb4000 <mathfan1980@gmail.com>
2021-01-29 11:42:00 -0500

    Added redis integration

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,20 @@
 import express from "express";
-
+import redis from "redis";
 const app = express();
 const port = 3000;
 app.use(express.static("public"));
 
 app.get("/test", (req, res) => {
-  res.send("The sedulous hyena ate the antelope!");
+
+const client = redis.createClient(6379, "redis-master");
+client.on("error", (error)=> {
+  console.error(error);
+});
+client.set("key", "value", redis.print);
+client.get("key", (err, value) => {
+  if (err) { throw err; }
+  res.send("The sedulous hyena ate the antelope poop key!" + value.toString()) ;
+});
 });
 app.listen(port, () => {
 
